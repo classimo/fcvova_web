8485a6424ca3b769d076f0595ecd2215ec35254b
classimo <grigaitis@gmail.com>
2016-08-08 20:56:21 +0100

    3

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
-  port: process.env.PORT || 8888,
+  mongoURL: process.env.MONGO_URL || 'mongodb://paulius:ALUSalus666@ds029655.mlab.com:29655/heroku_zcbm1p3w',
+  port: process.env.PORT || 80,
 };
 
 export default config;
