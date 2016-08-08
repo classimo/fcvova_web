f4f2dc2c240b44637f4e79b471ba0f859a32dc83
classimo <grigaitis@gmail.com>
2016-08-08 22:17:12 +0100

    Monday 4

---
+++ dist/0.server.bundle.js
@@ -0,0 +1,246 @@
+exports.ids = [0];
+exports.modules = {
+
+/***/ 84:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _jsx = function () { var REACT_ELEMENT_TYPE = typeof Symbol === "function" && Symbol.for && Symbol.for("react.element") || 0xeac7; return function createRawReactElement(type, props, key, children) { var defaultProps = type && type.defaultProps; var childrenLength = arguments.length - 3; if (!props && childrenLength !== 0) { props = {}; } if (props && defaultProps) { for (var propName in defaultProps) { if (props[propName] === void 0) { props[propName] = defaultProps[propName]; } } } else if (!props) { props = defaultProps || {}; } if (childrenLength === 1) { props.children = children; } else if (childrenLength > 1) { var childArray = Array(childrenLength); for (var i = 0; i < childrenLength; i++) { childArray[i] = arguments[i + 3]; } props.children = childArray; } return { $$typeof: REACT_ELEMENT_TYPE, type: type, key: key === undefined ? null : '' + key, ref: null, props: props, _owner: null }; }; }();
+
+// Import Style
+
+
+// Import Actions
+
+
+// Import Selectors
+
+
+exports.PostDetailPage = PostDetailPage;
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRedux = __webpack_require__(4);
+
+var _reactHelmet = __webpack_require__(7);
+
+var _reactHelmet2 = _interopRequireDefault(_reactHelmet);
+
+var _reactIntl = __webpack_require__(1);
+
+var _PostListItem = {
+  "single-post": "PostListItem__single-post__3B15Q",
+  "post-title": "PostListItem__post-title__3mZF-",
+  "author-name": "PostListItem__author-name__1cSDP",
+  "post-desc": "PostListItem__post-desc__3D8Fg",
+  "post-action": "PostListItem__post-action__3S84c",
+  "divider": "PostListItem__divider__y2SIF",
+  "post-detail": "PostListItem__post-detail__3W9vr"
+};
+
+var _PostListItem2 = _interopRequireDefault(_PostListItem);
+
+var _PostActions = __webpack_require__(85);
+
+var _PostReducer = __webpack_require__(86);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _ref = _jsx(_reactIntl.FormattedMessage, {
+  id: 'by'
+});
+
+function PostDetailPage(props) {
+  return _jsx('div', {}, void 0, _jsx(_reactHelmet2.default, {
+    title: props.post.title
+  }), _jsx('div', {
+    className: _PostListItem2.default['single-post'] + ' ' + _PostListItem2.default['post-detail']
+  }, void 0, _jsx('h3', {
+    className: _PostListItem2.default['post-title']
+  }, void 0, props.post.title), _jsx('p', {
+    className: _PostListItem2.default['author-name']
+  }, void 0, _ref, ' ', props.post.name), _jsx('p', {
+    className: _PostListItem2.default['post-desc']
+  }, void 0, props.post.content)));
+}
+
+// Actions required to provide data for this component to render in sever side.
+PostDetailPage.need = [function (params) {
+  return (0, _PostActions.fetchPost)(params.cuid);
+}];
+
+// Retrieve data from store as props
+function mapStateToProps(state, props) {
+  return {
+    post: (0, _PostReducer.getPost)(state, props.params.cuid)
+  };
+}
+
+exports.default = (0, _reactRedux.connect)(mapStateToProps)(PostDetailPage);
+
+/***/ },
+
+/***/ 85:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DELETE_POST = exports.ADD_POSTS = exports.ADD_POST = undefined;
+exports.addPost = addPost;
+exports.addPostRequest = addPostRequest;
+exports.addPosts = addPosts;
+exports.fetchPosts = fetchPosts;
+exports.fetchPost = fetchPost;
+exports.deletePost = deletePost;
+exports.deletePostRequest = deletePostRequest;
+
+var _apiCaller = __webpack_require__(35);
+
+var _apiCaller2 = _interopRequireDefault(_apiCaller);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Export Constants
+var ADD_POST = exports.ADD_POST = 'ADD_POST';
+var ADD_POSTS = exports.ADD_POSTS = 'ADD_POSTS';
+var DELETE_POST = exports.DELETE_POST = 'DELETE_POST';
+
+// Export Actions
+function addPost(post) {
+  return {
+    type: ADD_POST,
+    post: post
+  };
+}
+
+function addPostRequest(post) {
+  return function (dispatch) {
+    return (0, _apiCaller2.default)('posts', 'post', {
+      post: {
+        name: post.name,
+        title: post.title,
+        content: post.content
+      }
+    }).then(function (res) {
+      return dispatch(addPost(res.post));
+    });
+  };
+}
+
+function addPosts(posts) {
+  return {
+    type: ADD_POSTS,
+    posts: posts
+  };
+}
+
+function fetchPosts() {
+  return function (dispatch) {
+    return (0, _apiCaller2.default)('posts').then(function (res) {
+      dispatch(addPosts(res.posts));
+    });
+  };
+}
+
+function fetchPost(cuid) {
+  return function (dispatch) {
+    return (0, _apiCaller2.default)('posts/' + cuid).then(function (res) {
+      return dispatch(addPost(res.post));
+    });
+  };
+}
+
+function deletePost(cuid) {
+  return {
+    type: DELETE_POST,
+    cuid: cuid
+  };
+}
+
+function deletePostRequest(cuid) {
+  return function (dispatch) {
+    return (0, _apiCaller2.default)('posts/' + cuid, 'delete').then(function () {
+      return dispatch(deletePost(cuid));
+    });
+  };
+}
+
+/***/ },
+
+/***/ 86:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getPost = exports.getPosts = undefined;
+
+var _PostActions = __webpack_require__(85);
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+// Initial State
+var initialState = { data: [] };
+
+var PostReducer = function PostReducer() {
+  var state = arguments.length <= 0 || arguments[0] === undefined ? initialState : arguments[0];
+  var action = arguments[1];
+
+  switch (action.type) {
+    case _PostActions.ADD_POST:
+      return {
+        data: [action.post].concat(_toConsumableArray(state.data))
+      };
+
+    case _PostActions.ADD_POSTS:
+      return {
+        data: action.posts
+      };
+
+    case _PostActions.DELETE_POST:
+      return {
+        data: state.data.filter(function (post) {
+          return post.cuid !== action.cuid;
+        })
+      };
+
+    default:
+      return state;
+  }
+};
+
+/* Selectors */
+
+// Get all posts
+var getPosts = exports.getPosts = function getPosts(state) {
+  return state.posts.data;
+};
+
+// Get post by cuid
+var getPost = exports.getPost = function getPost(state, cuid) {
+  return state.posts.data.filter(function (post) {
+    return post.cuid === cuid;
+  })[0];
+};
+
+// Export Reducer
+exports.default = PostReducer;
+
+/***/ }
+
+};;
